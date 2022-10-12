2f80383ba49d04eabb20aad4c3ba5950cce2cb39
Scally-H <register@herczog.plus.com>
2022-10-12 17:03:16 +0100

    Updated for systemd service startup

--- RR_MQTT/RR_MQTT.py
+++ RR_MQTT/RR_MQTT.py
@@ -28,6 +28,7 @@ import yaml
 import signal
 import subprocess
 import threading
+import logging
 import os
 import time
 from json import dumps, loads, JSONDecodeError
@@ -57,7 +58,8 @@ I2C_ADDRESS = 0x40
 SHUNT_OHMS = 0.05
 MAX_EXPECTED_AMPS = 5.5
 BATTERY_ERR = 4.25
-BATTERY_VMAX = 4.2
+# Battery voltage may drop slightly after charging complete
+BATTERY_VMAX = 4.2 - 0.02
 
 # Define BATTERY_WARN % charge level at which to send immediate MQTT update
 BATTERY_WARN = 10
@@ -95,7 +97,7 @@ def mqtt_on_connect(mqtt_client, userdata, flags, rc):
     global client_connected
 
     if rc == 0:
-        print("Connected to MQTT broker")
+        logger.info("Connected to MQTT broker")
         client_connected = True
         mqtt_client.will_set(
             f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
@@ -103,31 +105,40 @@ def mqtt_on_connect(mqtt_client, userdata, flags, rc):
             qos=1,
             retain=True,
         )
-        mqtt_client.publish(
-            f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
-            payload=config['online'],
-            qos=1,
-            retain=True,
-        )
+        if rr_ina:
+            mqtt_client.publish(
+                f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
+                payload=config['online'],
+                qos=1,
+                retain=True,
+            )
+        else:
+            mqtt_client.publish(
+                f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
+                payload="RR_Startup_Error",
+                qos=1,
+                retain=True,
+            )
 
         # For testing, subscribe to all topics
-        print("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}")
-        mqtt_client.subscribe(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}")
-        print("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_DATA}")
-        mqtt_client.subscribe(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_DATA}")
+        if logging.DEBUG in [c_handler.level, f_handler.level]:
+            logger.debug("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}")
+            mqtt_client.subscribe(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}")
+            logger.debug("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_DATA}")
+            mqtt_client.subscribe(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_DATA}")
 
         # Subscribe to the command return topic
-        print("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_CMDS}")
+        logger.info("Subscribing to topic " + f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_CMDS}")
         mqtt_client.subscribe(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_CMDS}")
     else:
-        print("MQTT broker connection refused, error code = ", rc)
+        logger.error(f"MQTT broker connection refused, error code = {rc}")
 
 
 def mqtt_on_disconnect(mqtt_client, userdata, rc):
     """Handle temporary network connection issues"""
     global client_connected
 
-    print("Disconnected from MQTT broker: " + str(rc))
+    logger.warning(f"Disconnected from MQTT broker: {rc}")
     client_connected = False
 
 
@@ -141,46 +152,46 @@ def on_message(clientid, userdata, message):
     """
     global config, BATTERY_WARN, BATTERY_VMIN
 
-    print("message received ", str(message.payload.decode("utf-8")))
-    print("message topic=", message.topic)
-    print("message qos=", message.qos)
-    print("message retain flag=", message.retain)
+    logger.debug(f"message received {str(message.payload.decode('utf-8'))}")
+    logger.debug(f"message topic={message.topic}")
+    logger.debug(f"message qos={message.qos}")
+    logger.debug(f"message retain flag={message.retain}")
 
     # Commands expected as JSON format string
     try:
         message_data = loads(str(message.payload.decode("utf-8")))
     except JSONDecodeError:
-        print("message received ", str(message.payload.decode("utf-8")))
+        logger.warning(f"Non-JSON msg received: {str(message.payload.decode('utf-8'))}")
         return
 
     if "Shutdown" in message_data.keys():
-        print("Shutdown command received!")
+        logger.info("Shutdown command received!")
         mqtt_on_exit(0, 0, 1)
 
     if "Reboot" in message_data.keys():
-        print("Reboot command received!")
+        logger.info("Reboot command received!")
         mqtt_on_exit(0, 0, 2)
 
     if "Interval" in message_data.keys():
         try:
             config['publish_period'] = int(message_data['Interval'])
-            print(f"Changed reporting interval to {config['publish_period']}s")
+            logger.info(f"Changed reporting interval to {config['publish_period']}s")
         except ValueError:
-            print("Error changing publishing interval")
+            logger.error("Error changing publishing interval")
 
     if "WARN" in message_data.keys():
         try:
             BATTERY_WARN = float(message_data['WARN'])
-            print(f"Changed Warning Threshold to {BATTERY_WARN}V")
+            logger.info(f"Changed Warning Threshold to {BATTERY_WARN}V")
         except ValueError:
-            print("Error changing Warning Threshold")
+            logger.error("Error changing Warning Threshold")
 
     if "VMIN" in message_data.keys():
         try:
             BATTERY_VMIN = float(message_data['VMIN'])
-            print(f"Changed Shutdown Threshold to {BATTERY_VMIN}V")
+            logger.info(f"Changed Shutdown Threshold to {BATTERY_VMIN}V")
         except ValueError:
-            print("Error changing Shutdown Threshold")
+            logger.error("Error changing Shutdown Threshold")
 
 
 def mqtt_on_exit(signum, frame, exit_option=0):
@@ -190,10 +201,10 @@ def mqtt_on_exit(signum, frame, exit_option=0):
     """
     global stop_thread
 
-    print("Exiting ...")
+    logger.info("Exiting ...")
     stop_thread = True
 
-    print("Sending offline message")
+    logger.info("Sending offline message")
     client.publish(
         f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
         payload=config["offline"],
@@ -224,59 +235,68 @@ def publish_battery_status(ina, mqtt_client, stop):
     """
 
     shutdown = False
+    volts = BATTERY_ERR
+    current = 0
+    external_power = True
     last_publish = time.perf_counter()
 
     while not shutdown and not stop():
-        volts = ina.voltage()
+        if ina:
+            volts = ina.voltage()
         charge_level = int(max(min(100, (volts - BATTERY_VMIN) / (BATTERY_VMAX - BATTERY_VMIN) * 100), 0))
 
-        try:
-            # <0 is charging, <10 is FULL, >10 is discharging
-            current = ina.current()
-        except DeviceRangeError:
-            # Current out of device range with specified shunt resistor
-            # Assume no ext power so it will still shutdown on low voltage reading
-            external_power = False
-            current = 6000
-            mqtt_client.publish(
-                f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
-                payload="RR_READ_Error",
-                qos=1,
-                retain=True,
-            )
-
-        else:
-            # Identify status change
-            if current > 10:
-                # External power was removed
+        if ina:
+            try:
+                # <0 is charging, <10 is FULL, >10 is discharging
+                current = ina.current()
+            except DeviceRangeError:
+                # Current out of device range with specified shunt resistor
+                # Assume no ext power so it will still shutdown on low voltage reading
+                logger.error("Red Reactor Battery Current Range Error")
                 external_power = False
-                # Force immediate publish update
+                current = 6000
                 last_publish = 0
-            elif current >= 0:
-                # Battery now Full
-                external_power = True
-                # Force immediate publish update
-                last_publish = 0
+                mqtt_client.publish(
+                    f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
+                    payload="RR_Range_Error",
+                    qos=1,
+                    retain=True,
+                )
+
             else:
-                # Still charging
-                external_power = True
+                # Identify status change
+                if current > 10:
+                    # No External Power
+                    if external_power:
+                        # Power removed, publish immediately
+                        last_publish -= config['publish_period']
+                    external_power = False
+                elif current >= 0:
+                    # Battery now Full
+                    external_power = True
+                else:
+                    # Charging
+                    if not external_power:
+                        # Power restored, publish immediately
+                        last_publish -= config['publish_period']
+                    external_power = True
 
-            if charge_level <= BATTERY_WARN and not external_power:
-                # Force immediate publish update at warning level
-                last_publish = 0
+        if charge_level <= BATTERY_WARN and not external_power:
+            # Force immediate publish update at warning level
+            last_publish -= config['publish_period']
 
-            if charge_level == 0 and not external_power:
-                shutdown = True
+        if charge_level == 0 and not external_power:
+            shutdown = True
 
         if volts > BATTERY_ERR:
             external_power = True
-            # Force immediate publish update
-            last_publish = 0
+            # Force immediate publish update on battery error
+            last_publish -= config['publish_period']
 
         # Shutdown system
         if shutdown:
             # Go Offline and shutdown due to battery empty
-            print("Forcing system shutdown, going offline at {:.2f}volts".format(volts))
+            logger.info("Forcing system shutdown, going offline at {:.2f}volts".format(volts))
             mqtt_client.publish(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
                                 payload=config["offline"],
                                 qos=1,
@@ -289,10 +309,9 @@ def publish_battery_status(ina, mqtt_client, stop):
         else:
             # Publish status at required intervals and sleep till next battery check
             # State changes are published immediately
-            print("Battery Data: {},{:.2f},{:.2f},{},{}\n".format(time.strftime("%H:%M:%S"),
-                                                                  volts, current, charge_level, external_power))
+            logger.debug("Battery Data: {:.2f}v, {:.2f}mA, {}%, ExtPwr:{}".format(volts, current,
+                                                                                  charge_level, external_power))
             if time.perf_counter() - last_publish >= config['publish_period']:
-                print("Publishing new data")
                 last_publish = time.perf_counter()
                 # Add data from vcgencmd into status report
                 try:
@@ -304,6 +323,7 @@ def publish_battery_status(ina, mqtt_client, stop):
                     cpu_temp = float(cpu_data[0].decode().split("=")[1].replace("'C", ""))
                 except (OSError, IndexError, ValueError):
                     # Failed to extract info
+                    logger.error("Failed to read CPU info")
                     cpu_status = 65535
                     cpu_temp = 0
 
@@ -317,22 +337,64 @@ def publish_battery_status(ina, mqtt_client, stop):
                                          RR_VMIN=BATTERY_VMIN
                                          )
                 if client_connected:
+                    logger.info("Publishing new data")
                     mqtt_client.publish(f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_DATA}",
                                         dumps(rr_battery_status))
 
             # Wait for next status check, typically 5s
             time.sleep(READ_INTERVAL)
-    print("Exiting monitoring loop")
+    logger.debug("Exiting monitoring loop")
 
-
-print("Red Reactor MQTT Loading configuration")
-config = load_config(args.config_file)
 
 # Entry Point for MAIN program execution
 if __name__ == "__main__":
     """Red Reactor MQTT Client startup"""
 
-    print("Red Reactor MQTT Client setup")
+    # Create logger, enable all msgs
+    logger = logging.getLogger("RR_MQTT")
+    # Set to stop INA219 logging
+    logger.propagate = False
+    logger.setLevel(logging.DEBUG)
+
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('RR_MQTT.log')
+
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    logger.info("** RR_MQTT Startup **")
+    logger.info("Loading configuration")
+    config = load_config(args.config_file)
+
+    # Update logging levels, limited to listed set
+    logging_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    if 'c_log_level' in config.keys():
+        if config['c_log_level'] in logging_levels:
+            c_handler.setLevel(config['c_log_level'])
+            logger.info(f"Console log level set to {config['c_log_level']}")
+    if 'f_log_level' in config.keys():
+        if config['f_log_level'] in logging_levels:
+            f_handler.setLevel(config['f_log_level'])
+            logger.info(f"File log level set to {config['f_log_level']}")
+
+    # Verify that the RED REACTOR is attached
+    rr_ina = None
+    try:
+        rr_ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, busnum=1, log_level=logging.ERROR)
+        rr_ina.configure(rr_ina.RANGE_16V)
+    except (OSError, ModuleNotFoundError) as error:
+        # Log error but continue client (on_connect will send error status)
+        logger.error(f"** Unable to connect to the Red Reactor {error}")
+
+    logger.info("RR MQTT Client setup")
     client = mqtt.Client(client_id=HOST_NAME,
                          clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
     client.on_connect = mqtt_on_connect
@@ -346,32 +408,8 @@ if __name__ == "__main__":
         # An on_connect callback will set client_connected
         client.connect(config['mqtt']['broker'], config['mqtt']['port'], 60)
     except (ConnectionRefusedError, socket.timeout) as error:
-        print("** Unable to connect to the MQTT Broker,", error)
-        exit(1)
-
-    # Verify that the RED REACTOR is attached
-    rr_ina = None
-    try:
-        rr_ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, busnum=1)
-        rr_ina.configure(rr_ina.RANGE_16V)
-    except (OSError, ModuleNotFoundError) as error:
-        print("** Unable to connect to the Red Reactor,", error)
-        # Limited wait until connected to broker
-        client.loop_start()
-        for i in range(10):
-            if client_connected:
-                print("Sending error message")
-                client.publish(
-                    f"{config['hostname']}/{RR_SERVICE}/{RR_SERVICE_STATUS}",
-                    payload="RR_Startup_Error",
-                    qos=1,
-                    retain=True,
-                )
-                break
-            time.sleep(1)
-        # Ensure msg is sent
-        time.sleep(5)
-        exit(2)
+        # Log error but client loop will continue to try to connect
+        logger.error(f"** Unable to connect to the MQTT Broker {error}")
 
     # Run battery monitor in separate thread, which also publishes MQTT status updates
     stop_thread = False
